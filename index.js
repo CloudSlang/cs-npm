1475fa1e4917ab1038378d718cbfd22a5ce9faf4
Maayan Avraham <maayan.avraham@hpe.com>
2015-12-02 18:14:49 +0200

    Create index and package files

---
+++ index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+'use strict';
+
+console.log("Getting CloudSlang... it might take a while...");
+
+var fs = require("fs");
+var request = require("request");
+var AdmZip = require('adm-zip');
+var wrench = require("wrench");
+
+var cslangCliUrl = 'https://github.com/CloudSlang/cloud-slang/releases/download/cloudslang-0.8.7/cslang-cli.zip';
+var cslangZip = 'cslang-cli.zip';
+var cslangFolder = 'cloudslang-cli';
+
+var r = request(cslangCliUrl).pipe(fs.createWriteStream(cslangZip));
+
+r.on('finish', function () {
+    // reading archives 
+    var zip = new AdmZip(cslangZip);
+
+    // extracts everything 
+    zip.extractAllTo(cslangFolder, true);
+	wrench.chmodSyncRecursive(cslangFolder, '0755');
+	console.log("Finished");
+    
+});
