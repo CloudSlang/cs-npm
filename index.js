b5d1e25b8c81b7b1d6f6c0d2567ce039f8adf2e9
gituser173 <claudiu-laurian.ban@hp.com>
2016-03-18 14:22:48 +0200

    0.9.50
    Signed-off-by: Claudiu Ban claudiu-laurian.ban@hpe.com

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var request = require("request");
 var AdmZip = require('adm-zip');
 var wrench = require("wrench");
 
-var cslangCliUrl = 'https://github.com/CloudSlang/cloud-slang/releases/download/cloudslang-0.9.50.RC1/cslang-cli.zip';
+var cslangCliUrl = 'https://github.com/CloudSlang/cloud-slang/releases/download/cloudslang-0.9.50.0/cslang-cli.zip';
 var cslangZip = 'cslang-cli.zip';
 var cslangFolder = 'cloudslang-cli';
 
